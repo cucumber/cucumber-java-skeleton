214c19f884b4b91f58537affa433c1a1b66e7557
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-03-12 10:06:35 +0000

    First commit

---
+++ src/main/java/skeleton/Belly.java
@@ -0,0 +1,7 @@
+package skeleton;
+
+public class Belly {
+    public void eat(int cukes) {
+
+    }
+}
---
+++ src/test/java/skeleton/RunCukesTest.java
@@ -0,0 +1,10 @@
+package skeleton;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true)
+public class RunCukesTest {
+}
