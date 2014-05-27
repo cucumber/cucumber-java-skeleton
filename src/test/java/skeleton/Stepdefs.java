042d214959998912dbd5f90eab6b035ceb0d0277
Bruno Farina <brunofarinadev@yahoo.com.br>
2014-05-27 08:38:05 -0300

    Add new testing of features

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,6 +1,8 @@
 package skeleton;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
 
 public class Stepdefs {
     @Given("^I have (\\d+) cukes in my belly$")
@@ -8,4 +10,22 @@ public class Stepdefs {
         Belly belly = new Belly();
         belly.eat(cukes);
     }
+	
+	@When("^I wait (\\d+) hour$")
+	public void i_wait_hour(int hour) throws Throwable {
+	    Belly belly = new Belly();
+        belly.wait(hour);    
+	}
+
+	@Then("^my belly should growl$")
+	public void my_belly_should_growl() throws Throwable {
+		Belly belly = new Belly();
+		belly.growl();
+	}
+
+	@Then("^I will get fat$")
+	public void getting_fat() throws Throwable {
+		Belly belly = new Belly();
+		belly.gettingFat();
+	}		
 }
