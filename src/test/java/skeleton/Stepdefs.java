e90334f2b6933a6046e530dd0217ae19977b16c3
achmadns <achmad.dev@gmail.com>
2015-08-17 21:45:42 +0700

    Add needed steps to successfully run the example

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,11 +1,21 @@
 package skeleton;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class Stepdefs {
     @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("I wait 1 hour")
+    public void condition() {
+    }
+
+    @Then("my belly should growl")
+    public void check() {
     }
 }
