6c9c2f7d9183a3ba64146372a09485b31debfd4a
Justin Searls <searls@gmail.com>
2015-08-19 16:08:39 -0500

    monochrome setting for windows

--- src/test/java/bank/RunCukesTest.java
+++ src/test/java/bank/RunCukesTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty"}, monochrome = true)
 public class RunCukesTest {
   
   //This test is intentionally blank
