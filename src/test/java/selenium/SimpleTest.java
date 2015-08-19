712438a95349551b4955a5a8393caa1c688783eb
Justin Searls <searls@gmail.com>
2015-08-19 08:50:13 -0500

    Add selenium to the POM, sample test, gitignore

---
+++ src/test/java/selenium/SimpleTest.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleTest {
+
+  @Test
+  public void sometest() {
+    //http://www.seleniumhq.org/download/maven.jsp
+    //https://code.google.com/p/selenium/wiki/GettingStarted
+    
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.google.com");
+    WebElement element = driver.findElement(By.name("q"));
+    element.sendKeys("Cheese!");
+    element.submit();
+    System.out.println("Page title is: " + driver.getTitle());
+    driver.quit();
+  }
+  
+}
