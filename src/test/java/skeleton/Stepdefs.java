5783b99f8509868f70eedd80afedc8f74132471b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-26 07:45:05 +0000

    Add a chinese feature and stepdef

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,11 +1,17 @@
 package skeleton;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.zh_cn.假如;
 
 public class Stepdefs {
     @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @假如("^一世 开放 \"(.*?)\" 网址 在 该 浏览器$")
+    public void 一世_开放_网址_在_该_浏览器(String url) throws Throwable {
+        System.out.println("url = " + url);
     }
 }
---
+++ src/test/resources/skeleton/zh-CN.feature
@@ -0,0 +1,4 @@
+# language: zh-CN
+功能: 测试
+  场景: 注册负方案
+    而且 一世 开放 "https://accounts.coursera.org/signup" 网址 在 该 浏览器
