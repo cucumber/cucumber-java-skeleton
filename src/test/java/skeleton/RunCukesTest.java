e163b28ae2f91128c4d739e5a297f2779897072c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-02-01 11:37:34 +0000

    Add Cucumber Pro config (only activated in CI)

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty", "io.cucumber.pro.JsonReporter:all"})
 public class RunCukesTest {
 }
