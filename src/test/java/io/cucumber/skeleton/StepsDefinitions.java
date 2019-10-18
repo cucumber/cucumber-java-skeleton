292aa8db57157dd3432f2499e92b8cc9680a6166
M.P. Korstanje <logarithmicwhale@gmail.com>
2019-10-18 12:23:29 +0200

    Rename Steps to StepDefinitions

--- src/test/java/io/cucumber/skeleton/StepsDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepsDefinitions.java
@@ -2,7 +2,7 @@ package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
 
-public class Steps {
+public class StepsDefinitions {
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
