8eb0b0621cb5a409e1c5785aceb629043d420f45
M.P. Korstanje <rien.korstanje@gmail.com>
2020-01-09 16:52:51 +0100

    Use Cucumber v4.8.1

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -4,7 +4,7 @@ import io.cucumber.java.en.Given;
 
 public class StepDefinitions {
     @Given("I have {int} cukes in my belly")
-    public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
+    public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         belly.eat(cukes);
     }
