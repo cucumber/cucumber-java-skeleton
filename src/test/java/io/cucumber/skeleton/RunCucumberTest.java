7e0798367bcea613571dc9b9a591ade0cb89d135
M.P. Korstanje <logarithmicwhale@gmail.com>
2020-02-17 14:40:54 +0100

    Enable strict by default

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(strict =  true, plugin = {"pretty"})
 public class RunCucumberTest {
 }
