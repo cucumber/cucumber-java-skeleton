1fc297205cde038088d01381e90480267ebf082a
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-12-17 09:29:10 +0000

    Add html report

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-report.html"})
 public class RunCucumberTest {
 }
