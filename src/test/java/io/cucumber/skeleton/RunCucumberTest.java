90cc9852f37ff0b4278dfb8e0df01f41a5b4d680
M.P. Korstanje <rien.korstanje@gmail.com>
2021-12-11 13:34:46 +0100

    Improve first time user experience
     - Limit classpath scanning to `io.cucumber.skeleton` by setting the glue path.
       This avoids warnings about missing classes on Eclipse.
     - Quite Cucumber Publish plugin. The output on std-err is distracting from the
       message about missing step definitions.

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -6,10 +6,12 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("io/cucumber/skeleton")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
 public class RunCucumberTest {
 }
