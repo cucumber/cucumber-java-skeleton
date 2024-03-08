d96c72a6c1f32dbc00b00106529fb2ac60542ae6
M.P. Korstanje <rien.korstanje@gmail.com>
2024-03-08 19:13:10 +0100

    Switch to SelectPackages
    SelectClasspathResource is intended for single resources.

--- gradle/src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ gradle/src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -2,7 +2,7 @@ package io.cucumber.skeleton;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
@@ -10,7 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("io/cucumber/skeleton")
+@SelectPackages("io.cucumber.skeleton")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
 public class RunCucumberTest {
