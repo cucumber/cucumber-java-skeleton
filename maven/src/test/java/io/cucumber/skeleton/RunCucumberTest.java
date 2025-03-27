e9a9bb31394be7b6d5a66d186c14900edfe3eea9
M.P. Korstanje <rien.korstanje@gmail.com>
2025-03-27 14:31:34 +0100

    Update documentation on running single scenarios with Gradle

--- maven/src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ maven/src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -5,13 +5,11 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("io.cucumber.skeleton")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
 public class RunCucumberTest {
 }
